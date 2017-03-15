f72cc404b3e8a5824f286626c69d8d98b10293f6
Louis-Gabriel <louisgab@live.fr>
2017-03-15 15:58:58 +0100

    fin

--- systeme.c
+++ systeme.c
@@ -6,16 +6,12 @@
 #include "cpu.h"
 #include "systeme.h"
 
-
 int current_process = -1;
-
 int nbr_process_alive = 0;
 int nbr_process_sleeping = 0;
 int nbr_in_getchar = 0;
 int nbr_process = 0;
-
 int first_pc = 0;
-
 char tampon = '\0';
 char caractere = 'a';
 
@@ -24,98 +20,94 @@ time_t prochain_appel;
 /**********************************************************
 ** Programmes test
 ***********************************************************/
+void make_inst_test_fork(){
+    const int R3 = 3;
+	make_inst( 0, INST_NOP,   0,   0,   SYSC_IDLE);
+    make_inst( 1, INST_NOP,   0,   0,   0);
+    make_inst( 2, INST_NOP,   0,   0,   0);
+    make_inst( 3, INST_NOP,   0,   0,   0);
+    make_inst( 4, INST_JUMP,  0,   0,   0);
+    first_pc = SEGMENT_SIZE;
+    make_inst( first_pc + 0, INST_SYSC,  0,   0,   SYSC_FORK);
+    make_inst( first_pc + 1, INST_IFGT,  0,   0,   6);
+    make_inst( first_pc + 2, INST_SYSC,  R3,  0,   SYSC_PUTI);
+    make_inst( first_pc + 3, INST_NOP,   0,   0,   0);
+	make_inst( first_pc + 4, INST_SYSC,  R3,  0,   SYSC_PUTI);
+    make_inst( first_pc + 5, INST_SYSC,  0,   0,   SYSC_EXIT);
+    make_inst( first_pc + 6, INST_SUB,   R3,  R3,  -4);
+    make_inst( first_pc + 7, INST_SYSC,  R3,  0,   SYSC_SLEEP);
+    make_inst( first_pc + 8, INST_SUB,   R3,  R3,  0);
+    make_inst( first_pc + 9, INST_JUMP,  0,   0,   0);
+}
 
 void make_inst_test_getchar(){
 	const int R4 = 4, R3 = 3;
-
-	/*** Lecture d'un caractère et endormissement ***/
-
-	make_inst( 0, INST_NOP,  0, 0, SYSC_IDLE); /* boucle infinie idle */
-	make_inst( 1, INST_NOP,   0,   0, 0);
-	make_inst( 2, INST_NOP,   0,   0, 0);
-	make_inst( 3, INST_NOP,   0,   0, 0);
-	make_inst( 4, INST_JUMP, 0, 0, 0);
-
+	make_inst( 0, INST_SYSC,  0,   0,   SYSC_IDLE);
+	make_inst( 1, INST_NOP,   0,   0,   0);
+	make_inst( 2, INST_NOP,   0,   0,   0);
+	make_inst( 3, INST_NOP,   0,   0,   0);
+	make_inst( 4, INST_JUMP,  0,   0,   0);
 	first_pc = SEGMENT_SIZE;
-	make_inst( first_pc + 0, INST_SUB,   R3,  R3, -1);           /* R3 = 1         */
-	make_inst( first_pc + 1, INST_SYSC,  R4,  0, SYSC_GETCHAR); /* R4 = getchar() */
-	make_inst( first_pc + 2, INST_SYSC,  R4,  0, SYSC_PUTI);    /* puti(R4)       */
-	make_inst( first_pc + 3, INST_SYSC,  R3,  0, SYSC_SLEEP);   /* sleep(R3)      */
-	make_inst( first_pc + 4, INST_JUMP, 0, 0, 0);
+	make_inst( first_pc + 0, INST_SUB,   R3,  R3,  -1);
+	make_inst( first_pc + 1, INST_SYSC,  R4,  0,   SYSC_GETCHAR);
+	make_inst( first_pc + 2, INST_SYSC,  R4,  0,   SYSC_PUTI);
+	make_inst( first_pc + 3, INST_SYSC,  R3,  0,   SYSC_SLEEP);
+	make_inst( first_pc + 4, INST_JUMP,  0,   0,   0);
 }
 
 void make_inst_test_sleep(){
 	const int R3 = 3, R7 = 7;
-
-	/*** Exemple de création d'un thread ***/
-	make_inst( 0, INST_NOP,  0, 0, SYSC_IDLE); /* boucle infinie idle */
-	make_inst( 1, INST_NOP,   0,   0, 0);
-	make_inst( 2, INST_NOP,   0,   0, 0);
-	make_inst( 3, INST_NOP,   0,   0, 0);
-	make_inst( 4, INST_JUMP, 0, 0, 0);
-
-	first_pc = 5;
-
-	make_inst(first_pc +  0, INST_ADD,  R3,   0, 0);                 /* R3 = 1000    */
-	make_inst(first_pc +  1, INST_ADD,  R7,  R7, 2);
-	make_inst(first_pc +  2, INST_SYSC, R7,   0, SYSC_SLEEP);  /* créer un thread  */
-	make_inst(first_pc +  3, INST_SYSC, R3,   0, SYSC_PUTI);        /* afficher R3  */
-	make_inst(first_pc +  4, INST_NOP,   0,   0, 0);
-	make_inst(first_pc +  5, INST_NOP,   0,   0, 0);
-	make_inst(first_pc +  6, INST_NOP,   0,   0, 0);
-	make_inst(first_pc +  7, INST_NOP,   0,   0, 0);
-	make_inst(first_pc +  8, INST_NOP,   0,   0, 0);
-	make_inst(first_pc +  9, INST_SYSC, R7,   0, SYSC_SLEEP);  /* créer un thread  */
-	make_inst(first_pc +  10, INST_HALT,  0,   0, 0);
-
+	make_inst( 0, INST_NOP,   0,   0,   SYSC_IDLE);
+	make_inst( 1, INST_NOP,   0,   0,   0);
+	make_inst( 2, INST_NOP,   0,   0,   0);
+	make_inst( 3, INST_NOP,   0,   0,   0);
+	make_inst( 4, INST_JUMP,  0,   0,   0);
+	first_pc = SEGMENT_SIZE;
+	make_inst(first_pc +  0,   INST_ADD,   R3,  0,   0);
+	make_inst(first_pc +  1,   INST_ADD,   R7,  R7,  2);
+	make_inst(first_pc +  2,   INST_SYSC,  R7,  0,  SYSC_SLEEP);
+	make_inst(first_pc +  3,   INST_SYSC,  R3,  0,  SYSC_PUTI);
+	make_inst(first_pc +  4,   INST_NOP,   0,   0,  0);
+	make_inst(first_pc +  5,   INST_NOP,   0,   0,  0);
+	make_inst(first_pc +  6,   INST_NOP,   0,   0,  0);
+	make_inst(first_pc +  7,   INST_NOP,   0,   0,  0);
+	make_inst(first_pc +  8,   INST_NOP,   0,   0,  0);
+	make_inst(first_pc +  9,   INST_SYSC,  R7,  0,  SYSC_SLEEP);
+	make_inst(first_pc +  10,  INST_HALT,  0,   0,  0);
 }
-
-
 
 void make_inst_test_thread(){
 	const int R1 = 1, R3 = 3;
-
-	/*** Exemple de création d'un thread ***/
-	make_inst(first_pc +  0, INST_SYSC,  R1, R1, SYSC_NEW_THREAD);  /* créer un thread  */
-	make_inst(first_pc +  1, INST_IFGT,   0,  0, 10);               /* le père va en 10 */
-
-	/*** code du fils ***/
-	make_inst(first_pc +  2, INST_LOAD,  R3, 0, 0);                 /* R3 = 1000    */
-	make_inst(first_pc +  3, INST_SYSC,  R3,  0, SYSC_PUTI);        /* afficher R3  */
-	make_inst(first_pc +  4, INST_NOP, 0,  0, 0);
-	make_inst(first_pc +  5, INST_NOP,   0,   0, 0);
-	make_inst(first_pc +  6, INST_NOP,   0,   0, 0);
-	make_inst(first_pc +  7, INST_NOP,   0,   0, 0);
-	make_inst(first_pc +  8, INST_NOP,   0,   0, 0);
-	make_inst(first_pc +  9, INST_HALT,   0,   0, 0);
-
-	/*** code du père ***/
-	make_inst(first_pc + 10, INST_SUB, R3, R3, -3000);             /* R3 = 2000     */
-	make_inst(first_pc + 11, INST_STORE, R3,  0, 0);               /* afficher R3   */
-	make_inst(first_pc + 12, INST_SYSC,   0,  0, SYSC_EXIT);       /* fin du thread */
+	make_inst(first_pc +  0, INST_SYSC,  R1, R1,   SYSC_NEW_THREAD);
+	make_inst(first_pc +  1, INST_IFGT,  0,   0,   10);
+	make_inst(first_pc +  2, INST_LOAD,  R3,  0,   0);
+	make_inst(first_pc +  3, INST_SYSC,  R3,  0,   SYSC_PUTI);
+	make_inst(first_pc +  4, INST_NOP,   0,   0,   0);
+	make_inst(first_pc +  5, INST_NOP,   0,   0,   0);
+	make_inst(first_pc +  6, INST_NOP,   0,   0,   0);
+	make_inst(first_pc +  7, INST_NOP,   0,   0,   0);
+	make_inst(first_pc +  8, INST_NOP,   0,   0,   0);
+	make_inst(first_pc +  9, INST_HALT,  0,   0,   0);
+	make_inst(first_pc + 10, INST_SUB,   R3,  R3,  -3000);
+	make_inst(first_pc + 11, INST_STORE, R3,  0,   0);
+	make_inst(first_pc + 12, INST_SYSC,  0,   0,   SYSC_EXIT);
 }
 
 void make_inst_multi_thread(){
 	const int R1 = 1, R3 = 3;
-
-	/*** Exemple de création d'un thread ***/
-	make_inst( 0, INST_SYSC,  R1, R1, SYSC_NEW_THREAD);  /* créer un thread  */
-	make_inst( 1, INST_IFGT,   0,  0, 10);               /* le père va en 10 */
-
-	/*** code du fils ***/
-	make_inst( 2, INST_SUB,   R3, R3, -1000);            /* R3 = 1000    */
-	make_inst( 3, INST_SYSC,  R3,  0, SYSC_PUTI);        /* afficher R3  */
-	make_inst( 4, INST_NOP,   0,   0, 0);
-	make_inst( 5, INST_NOP,   0,   0, 0);
-	make_inst( 6, INST_NOP,   0,   0, 0);
-	make_inst( 7, INST_NOP,   0,   0, 0);
-	make_inst( 8, INST_NOP,   0,   0, 0);
-	make_inst( 9, INST_NOP,   0,   0, 0);
-
-	/*** code du père ***/
-	make_inst(10, INST_SUB,   R3, R3, -2000);           /* R3 = 2000     */
-	make_inst(11, INST_SYSC,  R3,  0, SYSC_PUTI);       /* afficher R3   */
-	make_inst(12, INST_SYSC,   0,  0, SYSC_EXIT);       /* fin du thread */
+	make_inst( 0, INST_SYSC,  R1,  R1,  SYSC_NEW_THREAD);
+	make_inst( 1, INST_IFGT,  0,   0,   10);
+	make_inst( 2, INST_SUB,   R3,  R3,  -1000);
+	make_inst( 3, INST_SYSC,  R3,  0,   SYSC_PUTI);
+	make_inst( 4, INST_NOP,   0,   0,   0);
+	make_inst( 5, INST_NOP,   0,   0,   0);
+	make_inst( 6, INST_NOP,   0,   0,   0);
+	make_inst( 7, INST_NOP,   0,   0,   0);
+	make_inst( 8, INST_NOP,   0,   0,   0);
+	make_inst( 9, INST_NOP,   0,   0,   0);
+	make_inst(10, INST_SUB,   R3,  R3,  -2000);
+	make_inst(11, INST_SYSC,  R3,  0,   SYSC_PUTI);
+	make_inst(12, INST_SYSC,  0,   0,   SYSC_EXIT);
 }
 
 
@@ -129,13 +121,12 @@ static PSW systeme_init(void) {
 	prochain_appel = time(NULL) + 4;
 
 	/*** creation d'un programme ***/
-	//make_inst_multi_thread(); //make_inst_test_thread();
-  //make_inst_test_sleep();
-	make_inst_test_getchar();
+	//make_inst_multi_thread();
+    make_inst_test_thread();
+    //make_inst_test_sleep();
+	//make_inst_test_fork();
 
 	/*** valeur initiale du PSW ***/
-
-	/*** Pas de process ***/
 	for(int i = 0 ; i < MAX_PROCESS ; i++){
 		process[i].state = EMPTY;
 	}
@@ -145,7 +136,6 @@ static PSW systeme_init(void) {
 	process[0].cpu.PC = 0;
 	process[0].cpu.SB = 0;
 	process[0].cpu.SS = SEGMENT_SIZE;
-
 	nbr_process_alive++;
 	nbr_process++;
 
@@ -156,9 +146,7 @@ static PSW systeme_init(void) {
 	process[1].state = READY;
 	nbr_process_alive++;
 	nbr_process++;
-
 	current_process = 1;
-
 
 	return process[1].cpu;
 }
@@ -170,19 +158,19 @@ PSW systeme_init_boucle(void) {
     printf("Booting (avec boucle).\n");
 
     /*** creation d'un programme ***/
-    make_inst( 0, INST_ADD,  R1, R1, 0);    /* R1 = 0              */
-    make_inst( 1, INST_ADD,  R2, R2, 10);   /* R2 = 1000           */
-    make_inst( 2, INST_ADD,  R3, R3, 5);    /* R3 = 5              */
-    make_inst( 3, INST_CMP,  R1, R2, 0);    /* AC = (R1 - R2)      */
-    make_inst( 4, INST_IFGT,  0,  0, 11);   /* if (AC > 0) PC = 11 */
-    make_inst( 5, INST_NOP,   0,  0, 0);    /* no operation        */
-    make_inst( 6, INST_LOAD,   R2,  R3, 1);    /* no operation        */
-    make_inst( 7, INST_NOP,   0,  0, 0);    /* no operation        */
-    make_inst( 8, INST_ADD,  R1, R3, 0);    /* R1 += R3            */
-    make_inst( 9, INST_SYSC,  R1,  0, SYSC_PUTI);    /* SYSCALL    */
-	  make_inst(10, INST_SYSC,  R1,  0, SYSC_NEW_THREAD);    /* SYSCALL    */
-    make_inst(11, INST_JUMP,  0,  0, 3);    /* PC = 3              */
-    make_inst(12, INST_HALT,  0,  0, 0);    /* HALT                */
+    make_inst( 0, INST_ADD,  R1, R1, 0);
+    make_inst( 1, INST_ADD,  R2, R2, 10);
+    make_inst( 2, INST_ADD,  R3, R3, 5);
+    make_inst( 3, INST_CMP,  R1, R2, 0);
+    make_inst( 4, INST_IFGT,  0,  0, 11);
+    make_inst( 5, INST_NOP,   0,  0, 0);
+    make_inst( 6, INST_LOAD,   R2,  R3, 1);
+    make_inst( 7, INST_NOP,   0,  0, 0);
+    make_inst( 8, INST_ADD,  R1, R3, 0);
+    make_inst( 9, INST_SYSC,  R1,  0, SYSC_PUTI);
+	make_inst(10, INST_SYSC,  R1,  0, SYSC_NEW_THREAD);
+    make_inst(11, INST_JUMP,  0,  0, 3);
+    make_inst(12, INST_HALT,  0,  0, 0);
 
     /*** valeur initiale du PSW ***/
     memset (&cpu, 0, sizeof(cpu));
@@ -192,14 +180,14 @@ PSW systeme_init_boucle(void) {
     return cpu;
 }
 
-/********************************************************
+/**********************************************************
 ** Affichage registre PC
 ***********************************************************/
 void print_PC(PSW m){
 	printf("Program counter : %d\n", m.PC);
 }
 
-/********************************************************
+/**********************************************************
 ** Affichage registre DR
 ***********************************************************/
 void print_DR(PSW m){
@@ -240,11 +228,6 @@ PSW ordonnanceur(PSW m){
 		process[current_process].cpu = m;
 	}
 
-	/*if(nbr_process_alive == 1){
-		current_process = 0;
-		return process[0].cpu;
-	}*/
-
 	do{
 		current_process = (current_process + 1) % MAX_PROCESS;
 	}while(process[current_process].state != READY);
@@ -267,7 +250,6 @@ PSW new_thread(PSW m){
 
 	return m;
 }
-
 
 PSW send_thread_to_sleep(PSW m){
 	printf("Go to sleep current process %d\n", current_process);
@@ -314,14 +296,33 @@ PSW my_getchar(PSW m){
 
 PSW my_fork(PSW m){
 	printf("SYSC_FORK\n");
+    if(nbr_process >= MAX_PROCESS){
+        printf("Nombre max de process atteint, réessaye plus tard.\n");
+        return ordonnanceur(m);
+    }
 
-	return m;
+    int index = find_first_empty();
+    m.AC = index;
+
+    process[index].state = process[current_process].state;
+    memcpy(&process[index].cpu, &m, sizeof(m));
+    process[index].cpu.SB = index * SEGMENT_SIZE;
+    process[index].cpu.AC = 0;
+
+    nbr_process++;
+
+    for(int i = 0 ; i < m.SS ; i++){
+        mem[i + process[index].cpu.SB] = mem[i + m.SB];
+    }
+
+	return ordonnanceur(m);
 }
 
 PSW my_exit(PSW m){
 	printf("SYSC_EXIT : End of process\n");
 	process[current_process].state = EMPTY;
 	nbr_process_alive--;
+    nbr_process--;
 
 	return ordonnanceur(m);
 }
@@ -348,8 +349,6 @@ PSW system_SYSC(PSW m){
 			printf("Unknown ARG of SYSC\n");
 			break;
 	}
-
-
 	return m;
 }
 
@@ -357,9 +356,7 @@ PSW system_SYSC(PSW m){
 /**********************************************************
 ** Simulation du systeme (mode systeme)
 ***********************************************************/
-
 PSW systeme(PSW m) {
-	//printf("Courrant : %ld ############## prochain_appel = %ld\n", time(NULL), prochain_appel);
 	if(prochain_appel <= time(NULL) && m.IN != INT_INIT){
 		prochain_appel = time(NULL) + 4;
 		frappe_clavier();
